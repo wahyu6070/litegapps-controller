1b644cc9b625eba0ed00cb93e13cb7dde4a8a738
wahyu6070 <gwtsendangrejo6070@gmail.com>
2021-03-17 18:58:34 +0700

    added addon.d and rm bin

--- customize.sh
+++ customize.sh
@@ -1,10 +1,14 @@
-# LiteGapps
+# LiteGapps controller
 # By wahyu6070
 [ -d /data/litegapps ] && rm -rf /data/litegapps
-print "- copying binary"
+print "- Installing litegapps controller files"
 [ -d /data/litegapps ] && rm -rf /data/litegapps
 cp -af $MODPATH/litegapps /data/
 chmod -R 755 /data/litegapps
+print "- copying addon.d"
+[ -d $system/addon.d/28-litegapps-controller.sh ] && $system/addon.d/28-litegapps-controller.sh
+cp -pf $MODPATH/addon/28-litegapps-controller.sh $system/addon.d/
+chmod 755 $system/addon.d/28-litegapps-controller.sh
 print
 print
 print "*Tips"
